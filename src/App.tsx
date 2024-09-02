5dc477a3c8c650d47699fff8e40de08df378e0d4
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-09-02 09:12:06 -0300

    Criando a estilização do display

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Button from "components/Button/Button";
+import Input from "components/Input/Input";
 import { Container, Content } from "styles";
 
 function App() {
   return (
     <Container>
       <Content>
-        Olá
+        <Input />
       </Content>
     </Container>
   );
---
+++ src/components/Input/styles.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+ 
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  
+
+
+  input {
+    width: 100%;
+    height: 75px;
+    border: none;
+    background-color: #aff6af;
+
+    text-align: right;
+    font-size: 24px;
+    font-family: 'Roboto';
+  }
+`
